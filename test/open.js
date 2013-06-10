7b600e779d5ae23e12d061ac6783adff8dc5e6ef
PG <smartPG@gmail.com>
2013-06-11 00:56:14 +0800

    http://nodejs.org/api/"

--- test/open.js
+++ test/open.js
@@ -1,20 +1,17 @@
 // Use 'page.injectJs()' to load the script itself in the Page context
 
-if ( typeof(phantom) !== "undefined" ) {
-    var page = require('webpage').create();
 
- 
-    page.onConsoleMessage = function(msg) {
-        console.log(msg);
-    };
-    
-    page.onAlert = function(msg) {
-        console.log(msg);
-    };
-    page.open("http://99manga.com/manga/168/110433.htm?s=9", function(status) {
-        
-        phantom.exit();
-    });
-} else {
-    alert("* Script running in the Page context.");
-}
+var page = require('webpage').create();
+
+
+page.onConsoleMessage = function(msg) {
+    console.log(msg);
+};
+
+page.onAlert = function(msg) {
+    console.log(msg);
+};
+page.open("http://nodejs.org/api/", function(status) {
+
+    phantom.exit();
+});
